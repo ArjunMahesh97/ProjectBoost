2765476244c3c015425aa57343371cca5d9bafe4
ArjunMahesh97 <arjunmahesh97@gmail.com>
2019-04-17 01:03:56 +0530

    rocket movement

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour {
 
+    Rigidbody rigidBody;
+
 	// Use this for initialization
 	void Start () {
-		
+        rigidBody = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
@@ -17,16 +19,18 @@ public class Rocket : MonoBehaviour {
 
     private void ProcessInput()
     {
-        if(Input.GetKey(KeyCode.Space)){
-
+        if(Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-
-        }else if (Input.GetKey(KeyCode.D))
+            rigidBody.transform.Rotate(Vector3.forward);
+        }
+        else if (Input.GetKey(KeyCode.D))
         {
-
+            rigidBody.transform.Rotate(-Vector3.forward);
         }
 
     }
