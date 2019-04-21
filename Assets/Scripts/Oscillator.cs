1506862e51b166a441ea5edf475394afae23a1dd
ArjunMahesh97 <arjunmahesh97@gmail.com>
2019-04-21 11:49:35 +0530

    Manual oscillation

---
+++ Assets/Scripts/Oscillator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscillator : MonoBehaviour {
+
+    [SerializeField] Vector3 movementvector;
+
+    [Range(0, 1)] [SerializeField] float movementFactor = 0f;
+
+    Vector3 startingPos;
+
+	// Use this for initialization
+	void Start () {
+        startingPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 offset = movementvector * movementFactor;
+        transform.position = startingPos + offset;
+	}
+}
