ad21f172e74f7ae44e085ff7e2548c5691bd2bbf
ArjunMahesh97 <arjunmahesh97@gmail.com>
2019-04-21 12:00:35 +0530

    Automatic oscillation

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class Oscillator : MonoBehaviour {
 
-    [SerializeField] Vector3 movementvector;
+    [SerializeField] Vector3 movementvector = new Vector3(10f, 10f, 10f);
+    [SerializeField] float period = 2f;
 
-    [Range(0, 1)] [SerializeField] float movementFactor = 0f;
+    float movementFactor = 0f;
 
     Vector3 startingPos;
 
@@ -17,6 +18,15 @@ public class Oscillator : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+        float cycles = Time.time / period;
+
+        const float tau = Mathf.PI * 2;
+
+        float rawSineWave = Mathf.Sin(cycles * tau);
+
+        movementFactor = (rawSineWave / 2f) + 0.5f;
+
         Vector3 offset = movementvector * movementFactor;
         transform.position = startingPos + offset;
 	}
